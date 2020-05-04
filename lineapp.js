c60fd4bae3a8f129c7b0bacf8d413d2838a46d99
勝呂 美歩 <suguromiho@suguro-no-MacBook-Air.local>
2020-05-05 01:27:46 +0900

    update

--- lineapp.js
+++ lineapp.js
@@ -8,7 +8,7 @@ const LINE_NOTIFY_API_URL = 'https://notify-api.line.me/api/notify';
 // 実際には、GitHub Actions の
 // run: LINE_TOKEN=${{secrets.LINE_TOKEN}} node action.js
 // という書き方で渡されています
-const LINE_NOTIFY_TOKEN = process.env.LINE_TOKEN;
+const LINE_NOTIFY_TOKEN =process.env.LINE_TOKEN;
 
 let config = {
     url: LINE_NOTIFY_API_URL,
---
+++ time.js
@@ -0,0 +1,18 @@
+let time = getHours();
+//getMinutes() 
+
+
+if(getHours == 1){
+    console.log("2");
+}else{
+    console.log("huhun");
+}
+
+// const myPoint = 90;
+// const passingScore = 80;//合格ライン
+
+// if(myPoint >= passingScore){
+//     console.log('合格');
+// }else{
+//     console.log('不合格');
+// }
